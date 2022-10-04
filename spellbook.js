055ca40a526618e8d35ee08cb12d0ed96af35f69
MemoryDreams <VincentPresscod@gmail.com>
2022-10-04 23:32:38 +0300

    Fixed layout and added spell

--- spellbook.js
+++ spellbook.js
@@ -9,7 +9,7 @@ function showTab(spell, tabName) {
     }
   
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tab");
+    tablinks = document.getElementById(spell).getElementsByClassName("tab");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
