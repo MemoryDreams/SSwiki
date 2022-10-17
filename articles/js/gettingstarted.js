2467c9910c55f4cc16de8cb7c4eb9779d2de1352
MemoryDreams <vincent.presscod@gmail.com>
2022-10-17 21:27:55 +0300

    File stracture tweak

--- articles/js/gettingstarted.js
+++ articles/js/gettingstarted.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+let screentext = document.getElementById('screentext').innerHTML;
+let buttons = document.getElementsByClassName('answer');
+
+function installSwitcher(val) {
+    switch (val) {
+        case 'yes':
+            break;
+        case 'no':
+            break;
+    }
+}
+
+function removeButtons() {
+    let i = 0;
+    for (i = 0; i <= buttons.length; i++) {
+        document.getElementById('controlpanel').querySelector('button').remove();
+    }
+}
