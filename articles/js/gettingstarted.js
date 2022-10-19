b810230fec71e448bd2124c3ae04a1dcac7b11f1
MemoryDreams <vincent.presscod@gmail.com>
2022-10-19 20:00:41 +0300

    First stage if getting started

--- articles/js/gettingstarted.js
+++ articles/js/gettingstarted.js
@@ -1,4 +1,4 @@
-let screentext = document.getElementById('screentext').innerHTML;
+let screentext = document.getElementById('screentext');
 let buttons = document.getElementsByClassName('answer');
 let controlpanel = document.getElementById('controlpanel');
 
@@ -7,11 +7,9 @@ function installSwitcher(val) {
         case 'yes':
             break;
         case 'no':
+            helpWithInstall();
             break;
     }
-    removeButtons();
-    addButton('that worked', 'yes');
-    addButton('that worked too', 'no');
 }
 
 function removeButtons() {
@@ -25,10 +23,32 @@ function addButton(text, val) {
     let button = document.createElement('button');
     button.className = 'answer';
     button.innerHTML = text;
-    button.setAttribute("onclick", "installSwitcher('" + val + "')");
+    button.setAttribute("onclick", val);
+    controlpanel.appendChild(button);
+}
+
+function openInNewTab(url) {
+    window.open(url, '_blank').focus();
+}
+
+function addLink(img, destination) {
+    let button = document.createElement('button');
+    button.className = 'answer';
+    button.setAttribute('href', destination);
+    button.setAttribute("onclick", "openInNewTab('" + destination + "')");
+    let image = document.createElement('img');
+    image.setAttribute('src', img);
+    button.appendChild(image);
     controlpanel.appendChild(button);
 }
 
 function helpWithInstall() {
+    screentext.innerHTML = "Для начала Вам нужно скачать лаунчер. Нажмите на кнопку с иконкой внизу, чтобы перейти на официальный сайт Byond.<br><br>Пока Вы там, нажмите кнопку 'Register' сверху и заведите аккаунт.<br><br>По возможности стоит воздержаться от использования сервиса Mail.ru."
+    removeButtons();
+    addLink('../images/BYOND_logo_big.png', 'https://www.byond.com/download/');
+    addButton("Готово! Что дальше?", 'letsBegin()')
+}
+
+function letsBegin() {
 
 }
